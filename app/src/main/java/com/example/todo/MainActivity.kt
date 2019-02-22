95f92661a3331a58acf3d9ef4eec1a49f508a85d
tengfei <tengfei599100@163.com>
2019-02-22 23:23:16 +0800

    update

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.todo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 主界面
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
