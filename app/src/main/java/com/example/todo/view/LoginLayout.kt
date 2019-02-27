92656ad06601e60183e73f57626892afbc6b78bd
tengfei <tengfei599100@163.com>
2019-02-27 22:18:35 +0800

    update

--- app/src/main/java/com/example/todo/view/LoginLayout.kt
+++ app/src/main/java/com/example/todo/view/LoginLayout.kt
@@ -13,17 +13,31 @@ import kotlinx.android.synthetic.main.activity_login.view.*
  */
 class LoginLayout(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
 
+    private var keyBoardShow = false
+
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         if (changed) {
             val width = right - left
             val height = bottom - top
             //如果宽高比小于 3:4 的话说明输入框弹出布局被压缩
-            if (width.toFloat() / height.toFloat() < 3F / 4F) {
+            if (height.toFloat() / width.toFloat() < 4F / 3F) {
                 post {
-
+                    login_logo_layout.visibility = INVISIBLE
+                    val params = login_weight_layout.layoutParams as LayoutParams
+                    params.weight = 1.5F
+                    keyBoardShow = true
+                    login_weight_layout.requestLayout()
                 }
             } else {
+                if (keyBoardShow) {
+                    post {
+                        login_logo_layout.visibility = visibility
+                        val params = login_weight_layout.layoutParams as LayoutParams
+                        params.weight = 6F
+                        login_weight_layout.requestLayout()
+                    }
+                }
 
             }
         }
