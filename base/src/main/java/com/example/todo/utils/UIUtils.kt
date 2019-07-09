90ce47046fbb10681a075df515dab83fb1c099e7
tengfei <tengfei599100@163.com>
2019-07-10 00:18:22 +0800

    update

---
+++ base/src/main/java/com/example/todo/utils/UIUtils.kt
@@ -0,0 +1,31 @@
+package com.example.todo.utils
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+
+/**
+ * @author tengfei
+ * date 2019/7/10 12:01 AM
+ * email tengfeigo@outlook.com
+ * description UI相关的工具类
+ */
+
+
+fun dip2px(context: Context, dip: Int): Int {
+    val density: Float = context.resources.displayMetrics.density
+    val px = (dip * density + 0.5F) as Int
+    return px
+}
+
+fun sp2px(context: Context, spValue: Float): Int {
+    val fontScale: Float = context.resources.displayMetrics.scaledDensity
+    return (spValue * fontScale + 0.5f) as Int
+}
+
+fun getColor(context: Context, colorId: Int): Int {
+    return context.resources.getColor(colorId, null)
+}
+
+fun getDrawable(context: Context, resId: Int): Drawable {
+    return context.resources.getDrawable(resId)
+}
