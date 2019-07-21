a83d65114b8210bfbffcdeae2ddfdec9ed89db6f
tengfei <tengfei599100@163.com>
2019-07-21 23:59:04 +0800

    update

---
+++ app/src/main/java/com/example/todo/LauncherActivity.kt
@@ -0,0 +1,21 @@
+package com.example.todo
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.example.todo.base.BaseActivity
+
+/**
+ * @author tengfei
+ * date 2019/7/21 11:14 PM
+ * email tengfeigo@outlook.com
+ * description
+ */
+class LauncherActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
