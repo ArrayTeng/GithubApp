f87a80ea1d1371db221f7ec0f34ca74d260399ef
TengfeiGo <tengfeigo@outlook.com>
2019-08-14 23:13:05 +0800

    MVP框架搭建

--- common/src/main/java/com/tengfei/common/test/TestClient.kt
+++ common/src/main/java/com/tengfei/common/test/TestClient.kt
@@ -1,26 +1,75 @@
 package com.tengfei.common.test
 
-import android.app.backup.BackupAgent
+import android.util.Log.v
 import java.io.BufferedReader
 import java.io.FileReader
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
 
 
 /**
  * @author tengfei
  * date 2019/8/11 2:17 PM
  * email tengfeigo@outlook.com
- * description
+ * description 注意 noArgs
  */
 
-fun main() {
+data class People(val name: String, var age: String) {
+    fun hello01() {
+        println("hello01")
+    }
 
-    BufferedReader(FileReader("hello.text")).use {
-
+    fun hello01(nameString: String) {
+        println(nameString)
     }
 }
 
-data class Person(var name: String, var age: String){
-    fun work(){}
+class NoPrimaryConstructor {
+
+    constructor()
+
+    constructor(i: Int)
+
+    constructor(i: Int, name: String)
+
+}
+
+fun main() {
+
+//    // 1 Kotlin中使用Kotlin反射获取class对象的三种方式
+//    val people = People("feifei","23")
+//    val kClazz01:KClass<People> = People::class
+//    val kClazz02:KClass<out People> = people::class
+//    val kClazz03:KClass<People> = people.javaClass.kotlin
+//
+//    // 2 通过Kotlin反射构造对象
+//    // 2.1 通过主构造器来构造对象
+//    val kPrimaryConstructor:KFunction<People>? = kClazz01.primaryConstructor
+//    val kPeople = kPrimaryConstructor!!.call("kuku","25")
+//    print(kPeople)
+
+    // 3 没有主构造器只有从构造器的时候
+//    val clazz = NoPrimaryConstructor::class
+//    val noPrimaryConstructor01 = clazz.constructors.toMutableList()[0].call()
+//    val noPrimaryConstructor02 = clazz.constructors.toMutableList()[1].call(1)
+//    print(noPrimaryConstructor02)
+
+    // 4 在Kotlin中通过反射获取属性
+//    val kClazz: KClass<People> = People::class
+//    val people = kClazz.primaryConstructor!!.call("feifei","23")
+//    //获取People里属性为 name 的值
+//    (kClazz.memberProperties.first { it.name == "age" } as? KMutableProperty1<People,String>)?.set(people,"44")
+//    print(people)
+//
+    // 5 Kotlin中通过反射获取方法
+    val kClazz: KClass<People> = People::class
+    val people = kClazz.primaryConstructor!!.call("feifei", "23")
+    val method = kClazz.memberFunctions.toMutableList()[0].call()
+    print(method)
 }
 
 
--- mvp/src/main/java/com/tengfei/mvp/impl/BaseFragment.kt
+++ mvp/src/main/java/com/tengfei/mvp/impl/BaseFragment.kt
@@ -1,7 +1,12 @@
 package com.tengfei.mvp.impl
 
 import android.support.v4.app.Fragment
+import com.tengfei.mvp.IPresenter
 import com.tengfei.mvp.IView
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
 
 /**
  * @author tengfei
@@ -18,6 +23,24 @@ class BaseFragment<out P : BasePresenter<BaseFragment<P>>> : IView<P>, Fragment(
         presenter.view = this
     }
 
-    private fun createPresenter(): P = TODO()
+    private fun createPresenter(): P {
+        sequence {
+            var thisClass: KClass<*> = this@BaseFragment::class
+            while (true) {
+                //通过遍历当前类型的所有父类
+                yield(thisClass.supertypes)
+                thisClass = thisClass.supertypes.firstOrNull()?.jvmErasure ?: break
+            }
+        }.flatMap { it ->
+            it.flatMap {
+                it.arguments
+            }.asSequence()
+        }.first {
+            it.type?.jvmErasure?.isSubclassOf(IPresenter::class) ?: false
+        }.let {
+            @Suppress("UNCHECKED_CAST")
+            return it.type!!.jvmErasure.primaryConstructor!!.call() as P
+        }
+    }
 
 }
