ab6cad67f55d128e328b1c0f057f948ef20b0187
TengfeiGo <tengfeigo@outlook.com>
2019-08-28 18:59:29 +0800

    通用列表 2

---
+++ app/src/main/java/com/tengfei/github/view/common/CommonListFragment.kt
@@ -0,0 +1,112 @@
+package com.tengfei.github.view.common
+
+import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tengfei.github.R
+import com.tengfei.github.model.page.ListPage
+import com.tengfei.github.view.widget.ErrorInfoView
+import com.tengfei.mvp.impl.BaseFragment
+import kotlinx.android.synthetic.main.list.*
+import org.jetbrains.anko.support.v4.toast
+import retrofit2.adapter.rxjava.GitHubPaging
+
+/**
+ * @author tengfei
+ * date 2019-08-28 15:35
+ * email tengfeigo@outlook.com
+ * description
+ */
+
+abstract class CommonListFragment<DataType, out Presenter : CommonListPresenter<DataType,
+        CommonListFragment<DataType, Presenter>>> : BaseFragment<Presenter>() {
+
+    public abstract val adapter: CommonRecyclerAdapter<DataType>
+
+    public val errorInfoView by lazy {
+        ErrorInfoView(rootView)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        refreshView.setColorSchemeResources(R.color.google_blue, R.color.google_green, R.color.google_red, R.color.google_yellow)
+        refreshView.isRefreshing = true
+        fixRecyclerView.setLoadMoreEnabled(true)
+        fixRecyclerView.layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+        fixRecyclerView.itemAnimator = DefaultItemAnimator()
+
+        fixRecyclerView.setOnLoadMoreListener(presenter::loadMore)
+        refreshView.setOnRefreshListener(presenter::refreshData)
+
+        presenter.initData()
+
+    }
+
+    fun initData(data: GitHubPaging<DataType>) {
+        adapter.data.clear()
+        adapter.data.addAll(data)
+        refreshView.isRefreshing = false
+        //设置是否已加载全部
+        fixRecyclerView.setNoMore(data.isLast)
+        //一页加载的数量
+        fixRecyclerView.refreshComplete(ListPage.PAGE_SIZE)
+    }
+
+    fun initDataWithNothig() {
+        refreshView.isRefreshing = false
+        fixRecyclerView.setNoMore(true)
+        fixRecyclerView.refreshComplete(ListPage.PAGE_SIZE)
+        showError("No data")
+        errorInfoView.isClickable = false
+    }
+
+    fun initDataWithError(errorInfo: String) {
+        refreshView.isRefreshing = false
+        fixRecyclerView.setNoMore(true)
+        fixRecyclerView.refreshComplete(ListPage.PAGE_SIZE)
+        showError(errorInfo)
+        errorInfoView.isClickable = true
+        errorInfoView.setOnClickListener {
+            presenter.initData()
+        }
+    }
+
+    fun initdataWithMore(data: GitHubPaging<DataType>){
+        adapter.data.update(data)
+        fixRecyclerView.refreshComplete(ListPage.PAGE_SIZE)
+        fixRecyclerView.setNoMore(data.isLast)
+        dismissError()
+    }
+
+
+    fun ondataRefreshDataWithError(errorInfo: String) {
+        if (adapter.data.isEmpty()) {
+            showError(errorInfo)
+            errorInfoView.isClickable = true
+            errorInfoView.setOnClickListener {
+                presenter.initData()
+            }
+        } else {
+            toast("load fail")
+        }
+    }
+
+    fun refreshData(data: GitHubPaging<DataType>) {
+        initData(data)
+    }
+
+    open fun dismissError() {
+        errorInfoView.dismiss()
+    }
+
+    open fun showError(errorInfo: String) {
+        errorInfoView.show(errorInfo)
+    }
+}
---
+++ app/src/main/java/com/tengfei/github/view/common/CommonListPresenter.kt
@@ -0,0 +1,65 @@
+package com.tengfei.github.view.common
+
+import com.tengfei.github.model.page.ListPage
+import com.tengfei.mvp.impl.BasePresenter
+import rx.Subscription
+
+/**
+ * @author tengfei
+ * date 2019-08-28 15:35
+ * email tengfeigo@outlook.com
+ * description
+ */
+
+abstract class CommonListPresenter<DataType, out View : CommonListFragment<DataType, CommonListPresenter<DataType, View>>> : BasePresenter<View>() {
+
+
+    abstract val listPage: ListPage<DataType>
+
+    private val subscriptionList: ArrayList<Subscription> = ArrayList()
+
+    private var isRefresh: Boolean = true
+
+    fun initData() {
+        listPage.loadFromFirst().subscribe({
+            view.initData(it)
+        }, {
+            view.initDataWithError(it.message ?: "error")
+        }).let(subscriptionList::add)
+    }
+
+    fun loadMore() {
+        listPage.loadMore().subscribe({
+
+        }, {
+
+        }).let(subscriptionList::add)
+    }
+
+    fun refreshData() {
+        listPage.loadFromFirst().subscribe({
+            if (it.isEmpty()) {
+                view.initDataWithNothig()
+            } else {
+                view.refreshData(it)
+            }
+
+        }, {
+            view.ondataRefreshDataWithError(it.message ?: "error")
+        }).let(subscriptionList::add)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!isRefresh) {
+            refreshData()
+        }
+        isRefresh = false
+    }
+
+    override fun onDestroy() {
+        subscriptionList.forEach(Subscription::unsubscribe)
+        subscriptionList.clear()
+        super.onDestroy()
+    }
+}
